2ce1ad96c0ad0d302cbe4355ea58522c0b4bb024
praveen-9920 <114065074+praveen-9920@users.noreply.github.com>
2023-01-22 19:47:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,146 @@
+// let a = parseInt(prompt("enter your 1st number"))
+// let b = parseInt(prompt("enter your 2nd number"))
+// let c = parseInt(prompt("enter your 3rd number"))
+
+// if(a>b & b>c){
+//     console.log("1")
+// }
+// else{
+//     console.log("0")
+// }
+
+// if( userInput=="a" || 
+// userInput=="e" ||
+// userInput=="i" ||
+// userInput=="o" ||
+// userInput=="u"
+// ){
+//  console.log("it is a vowel")
+
+// }
+
+// else{
+//     console.log("it is a consonant")
+// }
+
+
+// let boy = "aditya"
+// boy = boy.split("")
+// boy = boy.reverse()
+// boy= boy.join("")
+// console.log(boy)
+
+// var a="h";
+// var b="i";
+ 
+//     // a would be swapped to b and b would be swapped to a
+//     [a,b]=[b,a];
+ 
+//     console.log(`after swap a= ${a}`);
+//     console.log(`after swap b= ${b}`);
+
+//     var f = "8"
+//     let a = 1
+//     console.log((+f) + a + 1)
+
+
+// array (objects )
+
+
+// console.log(fruits.splice(1,1));
+
+// console.log(fruits);
+// const fruits = ["mango", "apple","orange","banana"]
+
+// switch
+
+
+// let day=26;
+
+// switch (day) {
+//   case 0:
+//     day = "Sunday";
+//     break;
+//   case 1:
+//     day = "Monday";
+//     break;
+//   case 2:
+//     day = "Tuesday";
+//     break;
+//   case 3:
+//     day = "Wednesday";
+//     break;
+//   case 4:
+//     day = "Thursday";
+//     break;
+//   case 5:
+//     day = "Friday";
+//     break;
+//   case  6:
+//     day = "Saturday";
+
+//     break;
+// }
+
+// console.log();
+
+
+// for loop
+
+
+fruits= "mango"
+fruits = fruits.split("")
+console.log(fruits)
+fruits.reverse()
+for ( let i=0; i<fruits.length ;i++){
+    
+   console.log(fruits[i])
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// array methods
+
+// fruits.unshift("aditya", "praveen")
+
+// console.log(fruits)
+// console.log(fruits[0])
+
+// fruits[2] ="vishal"
+
+// console.log(fruits)
+
+// fruits.shift();
+// console.log(fruits)
+
+
+// const myname = "john"
+// myname ="aditya"
+// console.log(myname)
+
+
+
